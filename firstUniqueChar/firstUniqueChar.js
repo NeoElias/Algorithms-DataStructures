f88df97b79b6b64d39b12f42eab77d15bcda2011
Neo Maraisane <maraisanen@gmail.com>
2021-12-21 16:08:18 -0500

    Solution to first unique character in a string problem

---
+++ firstUniqueChar/firstUniqueChar.js
@@ -0,0 +1,19 @@
+var firstUniqChar = function(s) {
+    let chars = {};
+    for(let i =0; i < s.length; i++){
+        if(chars.hasOwnProperty(s[i])){
+           chars[s[i]]+=1;
+        }
+         else {
+             chars[s[i]] = 1
+         }
+    }
+ 
+     for(let i =0; i < s.length; i++) {
+         if(chars[s[i]] === 1) {
+             return i;
+         }
+         
+     }
+     return -1;
+};
